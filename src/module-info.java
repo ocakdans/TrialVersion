7bac4fc6bf1b70960d05a8daa37a26921d1b1be9
ocakdans <selim.ocakdan@gmail.com>
2020-11-09 20:57:00 +0300

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module TrialVersion {
+}
---
+++ src/package01/Class01.java
@@ -0,0 +1,13 @@
+package package01;
+
+public class Class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World.");
+		System.out.println("Hello everone");
+
+	}
+
+}
